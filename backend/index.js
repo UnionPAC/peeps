650415649ebbcb727ee1b14422a98c98efabd875
Geoff Jamieson <49350011+UnionPAC@users.noreply.github.com>
2023-08-07 15:21:44 -0400

    added middleware and dependencies

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,20 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
+import connectDb from "./config/db.js";
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+connectDb();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
-console.log(PORT);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  token = req.cookies.jwt;
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.userId).select("-password");
+      next();
+    } catch (err) {
+      console.error(err);
+      res.status(401);
+      throw new Error("Not authorized, Invalid Token!");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, No Token!");
+  }
+});
+
+export { protect };
