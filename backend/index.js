d54fdbc62bc5986b398f88b9d539ba9f28405bb5
Geoff Jamieson <49350011+UnionPAC@users.noreply.github.com>
2023-08-16 17:27:42 -0400

    chatRoutes & messageRoutes setup to start

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 import connectDb from "./config/db.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import chatRoutes from "./routes/chatRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 connectDb();
@@ -15,6 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/chats", chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
